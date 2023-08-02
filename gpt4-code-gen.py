e21328b97abd39856abc3ba40e00ffed5028c334
Thomas Mathew <thomas.matthew@gmail.com>
2023-08-02 16:58:24 -0400

    Update gpt4-code-gen.py

--- gpt4-code-gen.py
+++ gpt4-code-gen.py
@@ -6,6 +6,7 @@ rootDir = 'DIRECTORY_PATH'
 regex = re.compile(r'applicationDefaults\s*\{.*?\}', re.DOTALL)
 file_extension = 'FILE_EXTENSION'
 
+#Extract and print
 for dirName, subdirList, fileList in os.walk(rootDir):
     print(f'Found directory: {dirName}')
     for fname in fileList:
@@ -18,3 +19,19 @@ for dirName, subdirList, fileList in os.walk(rootDir):
                     print(f'File: {file_path}')
                     for match in matches:
                         print(match)
+
+text_to_remove = 'TEXT_TO_REMOVE'
+
+#remove text from file
+for dirName, subdirList, fileList in os.walk(rootDir):
+    print(f'Found directory: {dirName}')
+    for fname in fileList:
+        if fname.endswith(file_extension):
+            file_path = os.path.join(dirName, fname)
+            with open(file_path, 'r') as f:
+                lines = f.readlines()
+            with open(file_path, 'w') as f:
+                for line in lines:
+                    modified_line = line.replace(text_to_remove, '')
+                    if modified_line.strip():
+                        f.write(modified_line)
